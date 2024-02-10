34003204a6696ca4bdb9c0b98f2f6c45191952e7
Ross Clettenberg <raclettenberg@gmail.com>
2024-02-10 17:35:57 -0600

    Create gameboard (#2)
    * Add board array for ghosts. Add empty myBoard array. Add empty ghosts array. Create g object for game properties.
    
    * Add player object to app.js that controls game speed, game pause/start, and player position. Add an addEventListener method to the document object with the DOMContentLoaded event. Log message to the console.
    
    * Replace console log message in callback func with querySelector methods that initialize the game state once the document is fully loaded. These querySelectors select various classes  from the DOM and assign them to properties of the g object.
    
    * Add createGame func call to addEventListener callback. Add a tempBoard array with 1s, 2s, 3s. Use forEach method in createGame func to iterate over each element in tempBoard array and create a square. Use for loop to add the value of height to the cell with template literals. Set properties of grid columns and rows of g.grid to g.x to specify size of grid layout.
    
    * Add createSquare func to create a new div using the document.createElement method, and store this new div as a const named div. Use the classList.add method to add the class of box to it.
    
    * Pass val as an arg in createSquare func, and create a div and conditionally add a class based on if val is 1, val is 2, or val is 3. Then, add it to game board by appending to g.grid, and push to myBoard array to keep track.
    
    * Add div.t object property which is set to val argument. Add div.idVal object property which is set to length of myBoard array. Set a console directory of each div to list properties.
    
    * Add click event listener to console directory a div when clicked.

--- app.js
+++ app.js
@@ -1,1 +1,85 @@
-console.log("Hello World! Ready to go!");
+const board = ["pink", "blue", "green", "red", "purple", "orange"];
+
+const myBoard = [];
+
+const tempBoard = [
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+  1, 2, 3, 2, 2, 2, 2, 2, 2, 1,
+  1, 2, 1, 1, 1, 1, 1, 1, 2, 1,
+  1, 2, 2, 2, 2, 2, 2, 2, 2, 1,
+  1, 2, 1, 1, 1, 1, 1, 1, 2, 1,
+  1, 2, 2, 2, 2, 2, 2, 2, 2, 1,
+  1, 2, 1, 1, 1, 1, 1, 3, 2, 1,
+  1, 2, 2, 2, 2, 2, 2, 2, 2, 1,
+  1, 2, 1, 1, 1, 1, 1, 1, 2, 1,
+  1, 2, 2, 2, 2, 2, 2, 2, 2, 1,
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+];
+
+const ghosts = [];
+
+const g = {
+  x: "",
+  y: "",
+  h: 100,
+  size: 25,
+  ghosts: 6,
+  inplay: false,
+};
+
+const player = {
+  pos: 20,
+  speed: 4,
+  cool: 0,
+  pause: false,
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  g.grid = document.querySelector(".grid"); // game board
+  g.pacman = document.querySelector(".pacman"); // pacman parent element object
+  g.eye = document.querySelector(".eye"); // pacman child eye element to change pacman direction
+  g.mouth = document.querySelector(".mouth"); // pacman child mouth element to change pacman direction
+  g.ghost = document.querySelector(".ghost"); // ghost parent element object template
+  g.ghost.style.display = "none"; // hide ghost element.
+  createGame(); // create game board
+  console.log(g);
+});
+
+createGame = () => {
+  tempBoard.forEach((cell) => {
+    console.log(cell);
+    createSquare(cell);
+  });
+
+  for (let index = 0; index < g.size; index++) {
+    g.x += `${g.h}px `; // cell grid height
+  }
+  g.grid.style.gridTemplateColumns = g.x;
+  g.grid.style.gridTemplateRows = g.x;
+};
+
+createSquare = (val) => {
+  const div = document.createElement("div");
+  div.classList.add("box");
+  if (val == 1) {
+    div.classList.add("wall");
+  }
+  if (val == 2) {
+    const dot = document.createElement("div");
+    dot.classList.add("dot");
+    div.append(dot);
+  } // add dot to cell
+  if (val == 3) {
+    const dot = document.createElement("div");
+    dot.classList.add("power-pellet");
+    div.append(dot);
+  } // add power pellet to cell
+  g.grid.append(div);
+  myBoard.push(div);
+  div.t = val; // set cell type value based on tempBoard array index value
+  div.idVal = myBoard.length; // set cell id value based on myBoard array index
+  div.addEventListener(
+    "click", (e) => {
+      console.dir(div);
+    })
+};
