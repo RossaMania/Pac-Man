d587d324f7fd294688bd2037e968e76421666520
Ross Clettenberg <raclettenberg@gmail.com>
2024-02-10 14:47:04 -0600

    Add board array for ghosts. Add empty myBoard array. Add empty ghosts array. Create g object for game properties.

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Hello World! Ready to go!");
+const board = ["pink", "blue", "green", "red", "purple", "orange"];
+
+const myBoard = [];
+
+const ghosts = [];
+
+const g = {
+  x: "",
+  y: "",
+  h: 100,
+  size: 25,
+  ghosts: 6,
+  inplay: false,
+};
